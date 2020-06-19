cd65732bee8a9ed86409a77f4dddd4f96665378d
Jeroen Rinzema <post@jeroen.no>
2020-06-19 09:47:15 +0200

    Updated checksums

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -7,19 +7,19 @@ class Maestro < Formula
 
   if OS.mac?
     url "https://github.com/jexia/maestro/releases/download/v2.0.0-beta.14/maestro-2.0.0-beta.14-darwin-amd64.tar.gz"
-    sha256 "58c8fe6973d3fe3413af6f0ad35f945bda41fc578bea801f1d87ebbdc16977ba"
+    sha256 "e49ba0a8128dc6cc188fd1bde512097b5462f72240358ba1d3702c419fa38ab2"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/jexia/maestro/releases/download/v2.0.0-beta.14/maestro-2.0.0-beta.14-linux-amd64.tar.gz"
-      sha256 "e0ee6c82399fba78a31522ba8195d99aeecf67ce7639325a613a208a3b65cf9f"
+      sha256 "aaebdeaa74b54d4c9cf9cdd349553afbfcec41c13c2bb91222ad6915a7c2242c"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/jexia/maestro/releases/download/v2.0.0-beta.14/maestro-2.0.0-beta.14-linux-arm64.tar.gz"
-        sha256 "745c6bb369502a620b3bdf4b61631484d00a89f2080cc86cc8f72e4b3568c6d7"
+        sha256 "8512b80d487af6b450607d2dd6004ee73c80bdd91773f17fab24aa8870999938"
       else
         url "https://github.com/jexia/maestro/releases/download/v2.0.0-beta.14/maestro-2.0.0-beta.14-linux-arm.tar.gz"
-        sha256 "dc144be4fbb7294bfa8274345d64b4d756ca5376f5860295ad96fdad763a6535"
+        sha256 "521739ae574da940ba921d8ebf19eda6e40d171d41609dc550cefe56ccbecf66"
       end
     end
   end
